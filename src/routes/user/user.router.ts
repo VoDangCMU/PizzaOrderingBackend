a14d0957837d63f106373ab525846c455ee2a4f8
Tran Dinh Quan <trandinhquan.1803@gmail.com>
2025-02-18 17:04:33 +0700

    update get user by id

--- src/routes/user/user.router.ts
+++ src/routes/user/user.router.ts
@@ -1,20 +1,33 @@
 import {Request, Response} from "express";
 import {AppDataSource} from "@root/data-source";
-import User from "@root/entity/User";
+import User from "@root/entity/Users";
 import logger from "@root/logger";
 import express from "express";
+import {z} from "zod";
 
 const user = express.Router();
-user.get("/user/:id", getUserById);
+user.get("/:id", getUserById);
 module.exports = user;
+
+const UserIdSchema = z.object({
+    id: z.string().transform((val) => parseInt(val, 10)),
+})
 
 const UserRepository = AppDataSource.getRepository(User);
 
 export default async function getUserById (req: Request, res: Response) {
     const userId = req.params.id;
 
+    const parse = UserIdSchema.safeParse({id: userId});
+    if(parse.error){
+        res.BadRequest(parse.error);
+        return;
+    }
+
+    const userIdParsed = parse.data.id;
+
     UserRepository.findOne({
-        where: {id: Number(userId)},
+        where: {id: Number(userIdParsed)},
         select: ["id", "username", "dateOfBirth", "firstName", "lastName", "email", "phone", "address"],
     })
         .then(user => {
