6e8ac388f89194476c6e96c888d1346b9c617157
Tran Dinh Quan <trandinhquan.1803@gmail.com>
2025-02-20 21:00:16 +0700

    delete user by id

--- src/routes/user/user.router.ts
+++ src/routes/user/user.router.ts
@@ -8,6 +8,7 @@ import {z} from "zod";
 const user = express.Router();
 user.get("/:id", getUserById);
 user.put("/:id", updateUserById);
+user.delete("/:id", deleteUserById);
 module.exports = user;
 
 const UserIdSchema = z.object({
@@ -77,6 +78,32 @@ export async function updateUserById(req: Request, res: Response) {
             }
             Object.assign(user, parseBody.data);
             res.send(user);
+        })
+        .catch(err => {
+            logger.error(err);
+        })
+}
+
+export async function deleteUserById (req: Request, res: Response) {
+    const userId = parseInt(req.params.id, 10);
+
+    const parse = UserIdSchema.safeParse({id: userId});
+    if(parse.error){
+        res.BadRequest(parse.error);
+        return;
+    }
+    const userIdParsed = parse.data.id;
+
+    UserRepository.findOne({
+        where: {id: Number(userIdParsed)},
+    })
+        .then(user => {
+            if (!user) {
+                res.BadRequest("User not found");
+                return;
+            }
+            UserRepository.remove(user);
+            res.send("User deleted successfully");
         })
         .catch(err => {
             logger.error(err);
