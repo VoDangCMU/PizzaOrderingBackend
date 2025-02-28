803164bc7a959e77d6e8b05708e527ff99c15e9e
Tran Dinh Quan <trandinhquan.1803@gmail.com>
2025-02-28 22:10:44 +0700

    change file entity

--- src/entity/Ingredients.ts
+++ src/entity/Ingredients.ts
@@ -3,7 +3,7 @@ import {Entity, PrimaryGeneratedColumn, Column} from 'typeorm';
 export const INGREDIENT_TABLE_NAME = 'ingredients';
 
 @Entity({name: INGREDIENT_TABLE_NAME})
-export class Ingredients {
+export default class Ingredients {
     @PrimaryGeneratedColumn({type: "bigint"})
     id: number;
 
