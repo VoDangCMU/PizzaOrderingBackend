81ad897f021a3544eeed07103498b6c3e66825ee
Tran Dinh Quan <trandinhquan.1803@gmail.com>
2025-03-21 12:00:20 +0700

    get pizza by name

---
+++ src/routes/pizza/getPizzaByName.ts
@@ -0,0 +1,43 @@
+import {Request, Response} from 'express';
+import {z} from "zod";
+import {AppDataSource} from "@root/data-source";
+import Pizza from "@root/entity/Pizza";
+import logger from "@root/logger";
+
+const PizzaSchema = z.object({
+    name: z.string(),
+})
+
+const PizzaRepository = AppDataSource.getRepository(Pizza);
+
+export default function getPizzaByName(req: Request, res: Response) {
+    const name = req.query.name;
+    const parsed = PizzaSchema.safeParse(name);
+
+    if(parsed.error)
+    {
+        logger.warn(parsed.error);
+        res.BadRequest(parsed.error);
+        return;
+    }
+    const pizza = parsed.data;
+
+    PizzaRepository.findOne({
+        where: {
+            name: pizza.name,
+        }
+    })
+        .then(pizza =>{
+            if(!pizza)
+            {
+                res.NotFound("Pizza Not Found");
+                return;
+            }
+
+            res.Ok(pizza);
+        })
+        .catch(err => {
+            logger.error(err);
+            res.InternalServerError({});
+        })
+}
--- src/routes/pizza/pizza.router.ts
+++ src/routes/pizza/pizza.router.ts
@@ -5,10 +5,12 @@ import deletePizza from "@root/routes/pizza/deletePizza";
 import updatePizza from "@root/routes/pizza/updatePizza";
 import getAllPizzas from "@root/routes/pizza/getAllPizzas";
 import isAuth from "@root/middlewares/isAuth";
+import getPizzaByName from "@root/routes/pizza/getPizzaByName";
 
 const pizza = Router();
 
 pizza.get("/:id", getPizza);
+pizza.get("/", getPizzaByName);
 pizza.get("/",isAuth, getAllPizzas);
 pizza.post("/", createPizza);
 pizza.delete("/:id", deletePizza);
