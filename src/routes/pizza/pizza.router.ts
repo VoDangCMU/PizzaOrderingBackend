30383c19f01ae21f5dad9038b95e366bc22d1f7a
Tran Dinh Quan <trandinhquan.1803@gmail.com>
2025-03-27 09:59:37 +0700

    delete-auth-get

--- src/routes/pizza/pizza.router.ts
+++ src/routes/pizza/pizza.router.ts
@@ -4,15 +4,13 @@ import getPizza from "@root/routes/pizza/getPizza";
 import deletePizza from "@root/routes/pizza/deletePizza";
 import updatePizza from "@root/routes/pizza/updatePizza";
 import getAllPizzas from "@root/routes/pizza/getAllPizzas";
-import isAuth from "@root/middlewares/isAuth";
 import getPizzaByName from "@root/routes/pizza/getPizzaByName";
-import isAdmin from "@root/middlewares/isAdmin";
 
 const pizza = Router();
 
 pizza.get("/getByID/:id", getPizza);
 pizza.get("/getByName", getPizzaByName);
-pizza.get("/",isAuth, isAdmin, getAllPizzas);
+pizza.get("/", getAllPizzas);
 pizza.post("/", createPizza);
 pizza.delete("/:id", deletePizza);
 pizza.put("/", updatePizza);
