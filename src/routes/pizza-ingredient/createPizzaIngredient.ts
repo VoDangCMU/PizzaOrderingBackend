46d25c3507c1b2ce3a88924413aa64e4ae4467f0
NGUYEN BA KHOA <khoa.nguyenba@outlook.com>
2025-04-03 03:39:03 +0700

    refactor pizza-ingredient

--- src/routes/pizza-ingredient/createPizzaIngredient.ts
+++ src/routes/pizza-ingredient/createPizzaIngredient.ts
@@ -12,67 +12,67 @@ const IngredientRepository = AppDataSource.getRepository(Ingredients);
 const PizzaRepository = AppDataSource.getRepository(Pizza);
 
 export default async function createIngredient(req: Request, res: Response) {
-    const ingredientId = req.body.ingredientId;
-    const pizzaId = req.body.pizzaId;
-    let parsedIngredientId;
-    let parsedPizzaId;
+	const ingredientId = req.body.ingredientId;
+	const pizzaId = req.body.pizzaId;
+	let parsedIngredientId;
+	let parsedPizzaId;
 
-    try {
-        parsedIngredientId = z.union([z.string().regex(/^\d+$/), z.number()]).transform(Number).safeParse(ingredientId);
-        parsedPizzaId = z.union([z.string().regex(/^\d+$/), z.number()]).transform(Number).safeParse(pizzaId);
+	try {
+		parsedIngredientId = z.union([z.string().regex(/^\d+$/), z.number()]).transform(Number).safeParse(ingredientId);
+		parsedPizzaId = z.union([z.string().regex(/^\d+$/), z.number()]).transform(Number).safeParse(pizzaId);
 
-        if (parsedIngredientId.error) {
-            res.BadRequest(extractErrorsFromZod(parsedIngredientId.error));
-            return;
-        }
+		if (parsedIngredientId.error) {
+			res.BadRequest(extractErrorsFromZod(parsedIngredientId.error));
+			return;
+		}
 
-        if (parsedPizzaId.error) {
-            res.BadRequest(extractErrorsFromZod(parsedPizzaId.error));
-            return;
-        }
+		if (parsedPizzaId.error) {
+			res.BadRequest(extractErrorsFromZod(parsedPizzaId.error));
+			return;
+		}
 
-        const existedIngredient = await IngredientRepository.findOne({
-            where: {
-                id: parsedIngredientId.data,
-            }
-        })
-        if (!existedIngredient) {
-            res.NotFound("Ingredient not found");
-            return;
-        }
+		const existedIngredient = await IngredientRepository.findOne({
+			where: {
+				id: parsedIngredientId.data,
+			}
+		})
+		if (!existedIngredient) {
+			res.NotFound("Ingredient not found");
+			return;
+		}
 
-        const existedPizza = await PizzaRepository.findOne({
-            where: {
-                id: parsedPizzaId.data,
-            }
-        })
-        if (!existedPizza) {
-            res.NotFound("Pizza not found");
-            return;
-        }
+		const existedPizza = await PizzaRepository.findOne({
+			where: {
+				id: parsedPizzaId.data,
+			}
+		})
+		if (!existedPizza) {
+			res.NotFound("Pizza not found");
+			return;
+		}
 
-        const existedPizzaIngredient = await PizzaIngredientRepository.findOne({
-            where: {
-                pizza: { id: parsedPizzaId.data },
-                ingredient: { id: parsedIngredientId.data }
-            }
-        });
+		const existedPizzaIngredient = await PizzaIngredientRepository.findOne({
+			where: {
+				pizza: {id: parsedPizzaId.data},
+				ingredient: {id: parsedIngredientId.data}
+			}
+		});
 
-        if (existedPizzaIngredient) {
-            res.BadRequest([{message: "PizzaIngredient already exists", detail: existedPizzaIngredient}]);
-            return;
-        }
+		if (existedPizzaIngredient) {
+			res.BadRequest([{message: "PizzaIngredient already exists", detail: existedPizzaIngredient}]);
+			return;
+		}
 
-        const createdPizzaIngredient = new PizzaIngredient();
+		const createdPizzaIngredient = new PizzaIngredient();
 
-        createdPizzaIngredient.ingredient = existedIngredient;
-        createdPizzaIngredient.pizza = existedPizza;
+		createdPizzaIngredient.ingredient = existedIngredient;
+		createdPizzaIngredient.pizza = existedPizza;
 
-        await PizzaIngredientRepository.save(createdPizzaIngredient);
+		await PizzaIngredientRepository.save(createdPizzaIngredient);
 
-        res.Ok(createdPizzaIngredient);
-    } catch (error) {
-        logger.error(error);
-        res.InternalServerError({});
-    }
+		res.Ok(createdPizzaIngredient);
+	} catch (error) {
+		logger.error(error);
+		res.InternalServerError({});
+	}
 }
